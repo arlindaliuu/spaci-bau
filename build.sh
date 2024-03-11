7038134d77f795f5a1766337384acb6f48bcf3e3
Arlind Aliu <arlindaliu08@gmail.com>
2024-03-11 21:25:19 +0100

    created build.sh for automatically build

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Navigate to your theme directory on the live server
+cd /home/forumiri/public_html/wp-content/themes/spaci-bau
+
+# Pull changes from the Git repository
+git pull origin main
+
+# Install npm packages (if needed)
+# npm install
+
+# Run npm watch to compile JavaScript and SCSS files
+npm run watch &
+
+# Run grunt to watch Tailwind CSS and other styles
+grunt
