0c5abcae4b4b4a26be7c2f8ecb7eadbf5276e891
Arlind Aliu <arlindaliu08@gmail.com>
2024-03-14 02:45:01 +0100

    translated who we are

--- components/who-we-are.php
+++ components/who-we-are.php
@@ -4,17 +4,17 @@
         <div class="lg:col-span-2 relative flex flex-wrap justify-between">
             <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/pattern.jpg" alt="Pattern image" class="absolute top-0 lef-0 w-full h-full object-cover"/>
             <div class="relative p-10 grid lg:block">
-                <p class="uppercase sub-title">Who are we</p>
-                <span class="text-2xl lg:text-4xl font-bold mt-6 block">We value your trust & build since 1983</span>
+                <p class="uppercase sub-title">Wer sind wir</p>
+                <span class="text-2xl lg:text-4xl font-bold mt-6 block">Wir schätzen Ihr Vertrauen und bauen seit 1998 auf</span>
                 <p class="mt-6">
-                    Experience a level of our quality in both design & customization works. Whether it’s for you or by you, capture a good deed to get started.
-                    Manufacturing industry became a key sector of production and labour in European and North American countries during the Industrial Revolution, upsetting previous mercantile and feudal economies.
+                    Erleben Sie ein Niveau unserer Qualität sowohl bei Design- als auch bei Individualisierungsarbeiten. Ob für Sie oder von Ihnen, halten Sie zunächst eine gute Tat fest.
+                    Die verarbeitende Industrie wurde während der Industriellen Revolution zu einem Schlüsselsektor der Produktion und Arbeit in europäischen und nordamerikanischen Ländern und brachte die früheren Handels- und Feudalwirtschaften durcheinander.
                 </p>
-                <a href="/uber-uns" class="button-48 mt-6"><span>Learn more &#8594;</span></a>
+                <a href="/uber-uns" class="button-48 mt-6"><span>Erfahren Sie mehr &#8594;</span></a>
             </div>
             <div class="w-full relative grid grid-cols-2">
-                <p class="bg-yellow-500 font-bold flex justify-center items-center p-4 text-gray-900 text-md lg:text-xl"><span class="number-counter text-2xl lg:text-4xl mr-2">569</span> Total projects</p>
-                <p class="bg-gray-900 font-bold flex justify-center items-center p-4 text-white text-md lg:text-xl"><span class="number-counter text-2xl lg:text-4xl mr-2">120</span> qualified workers</p>
+                <p class="bg-yellow-500 font-bold flex justify-center items-center p-4 text-gray-900 text-md lg:text-xl"><span class="number-counter text-2xl lg:text-4xl mr-2">269</span> Gesamtprojekte</p>
+                <p class="bg-gray-900 font-bold flex justify-center items-center p-4 text-white text-md lg:text-xl"><span class="number-counter text-2xl lg:text-4xl mr-2">81</span> qualifizierte Arbeitskräfte</p>
             </div>
         </div>
         <div class="lg:col-span-1">
@@ -22,7 +22,7 @@
         </div>
     </div>
     <div class="max-w-5xl lg:mx-auto grid py-10 mx-6">
-        <p class="text-4xl">Making your vision come true at the basics.</p>
+        <p class="text-4xl">Verwirklichen Sie Ihre Vision im Wesentlichen.</p>
         <div class="grid lg:grid-cols-2 grid-cols-1 gap-4 mt-6">
             <div class="bg-white border rounded-3xl overflow-hidden">
                 <div class="border p-6 flex gap-6 justify-center items-center">
@@ -30,8 +30,8 @@
                         <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/construction.png" class="min-w-[40px] max-w-[40px] max-h-[40px] min-h-[40px]" alt="Construction Icon" />
                     </div>
                     <div>
-                        <p class="text-2xl font-semibold">General Construction</p>
-                        <span class="mt-6 block text-sm">Service overview With experience contracting and professional industry latest generation technological.</span>
+                        <p class="text-2xl font-semibold">Allgemeiner Bau</p>
+                        <span class="mt-6 block text-sm">Unser Team ist spezialisiert auf eine breite Palette von Bauarbeiten, einschließlich Wohn- und Gewerbebau, Renovierungen und strukturelle Verbesserungen.</span>
                     </div>
                 </div>
                 <div class="border p-6 flex gap-6 justify-center items-center">
@@ -39,8 +39,8 @@
                         <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/company.png" class="min-w-[40px] max-w-[40px] max-h-[40px] min-h-[40px]" alt="Construction Icon" />
                     </div>
                     <div>
-                        <p class="text-2xl font-semibold">Building Design</p>
-                        <span class="mt-6 block text-sm">ervice overview With experience contracting and professional industry latest generation technological.</span>
+                        <p class="text-2xl font-semibold">Gebäudedesign</p>
+                        <span class="mt-6 block text-sm">Mit einem Fokus auf Innovation und Funktionalität erstellen wir maßgeschneiderte Gebäudedesigns, die Ihren spezifischen Bedürfnissen und Vorlieben entsprechen und sowohl ästhetischen Reiz als auch Praktikabilität gewährleisten..</span>
                     </div>
                 </div>
                 <div class="border p-6 flex gap-6 justify-center items-center">
@@ -48,8 +48,8 @@
                         <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/project.png" class="min-w-[40px] max-w-[40px] max-h-[40px] min-h-[40px]" alt="Construction Icon" />
                     </div>
                     <div>
-                        <p class="text-2xl font-semibold">Project Management</p>
-                        <span class="mt-6 block text-sm">Service overview With experience contracting and professional industry latest generation technological.</span>
+                        <p class="text-2xl font-semibold">Projektmanagement</p>
+                        <span class="mt-6 block text-sm">Von der Konzeption bis zur Fertigstellung überwachen unsere erfahrenen Projektmanager jeden Aspekt Ihres Bauprojekts und gewährleisten Effizienz, Qualität und termingerechte Fertigstellung innerhalb des Budgetrahmens.</span>
                     </div>
                 </div>
             </div>
