ca85e1f6bd4a81934dd9fe5619fb807719c6815d
Arlind Aliu <arlindaliu08@gmail.com>
2024-04-01 01:26:14 +0200

    update contact-us

--- components/contact-us.php
+++ components/contact-us.php
@@ -1,7 +1,7 @@
 <div class="grid lg:grid-cols-3 relative items-center">
     <div class="bg-contact-form absolute w-full h-full z-10"></div>
     <div class="z-20 targetDiv animate-duration-[3000ms] grid gap-2">
-        <h3 class="text-xl lg:text-4xl p-6 pb-5 md:p-24 text-blue-50 md:text-white font-bold md:pb-0">Kontaktiere uns</h3>
+        <h2 class="text-xl lg:text-4xl p-6 pb-5 md:p-24 text-blue-50 md:text-white font-bold md:pb-0">Kontaktiere uns</h2>
         <p class="text-blue-50 md:text-white px-6 md:p-24 md:pt-0 font-normal text-lg md:text-xl">Wir schätzen Ihr Feedback und freuen uns immer, von Ihnen zu hören. Egal, ob Sie eine Frage oder einen Vorschlag haben oder einfach nur Hallo sagen möchten, wir sind für Sie da. Bitte kontaktieren Sie uns!</p>
     </div>
     <div class="col-span-2" >
